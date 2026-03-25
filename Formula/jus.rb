955b6566741c59f777260f79d9495862bb33ddc5
caleon <caleon@users.noreply.github.com>
2026-03-25 14:10:29 -0700

    Update jus formula to v0.2.0

--- Formula/jus.rb
+++ Formula/jus.rb
@@ -7,26 +7,26 @@ class Jus < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/juscribe/jus-agent/releases/download/v#{version}/jus-agent-darwin-arm64"
-      sha256 "PLACEHOLDER"
+      sha256 "a05abe9f6bcda4a692212ffc635016e090db311ebdd528a563cea487be07a757"
     else
       url "https://github.com/juscribe/jus-agent/releases/download/v#{version}/jus-agent-darwin-amd64"
-      sha256 "PLACEHOLDER"
+      sha256 "624144ff175c809a2862c362acd46bb75472859b6c9ce90590a38d702b6d7a00"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/juscribe/jus-agent/releases/download/v#{version}/jus-agent-linux-arm64"
-      sha256 "PLACEHOLDER"
+      sha256 "93fee53461becff5e94aae9568dbedc6c43ec7c1b3b96d33b8b8c8072aae6c17"
     else
       url "https://github.com/juscribe/jus-agent/releases/download/v#{version}/jus-agent-linux-amd64"
-      sha256 "PLACEHOLDER"
+      sha256 "a0b0e1ca89b9136c369d0a575f5f026aa2e246774930b0180f4afa3c4f46a1c1"
     end
   end
 
   resource "jus-cli" do
     url "https://github.com/juscribe/jus-agent/releases/download/v#{stable.version}/jus"
-    sha256 "PLACEHOLDER"
+    sha256 "23138839a143df2194dc1270c39df26d92f5f4a1c5591228ed1a59fb3c44fc8d"
   end
 
   def install
@@ -45,4 +45,3 @@ class Jus < Formula
     assert_match version.to_s, shell_output("#{bin}/jus-agent --version 2>&1")
   end
 end
-
