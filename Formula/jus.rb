04047a1b58f63bc3c820309363355694728f9e72
caleon <caleon@users.noreply.github.com>
2026-03-25 21:10:28 -0700

    Update jus formula to v0.3.4

--- Formula/jus.rb
+++ Formula/jus.rb
@@ -1,32 +1,32 @@
 class Jus < Formula
   desc "Juscribe CLI — project management for AI-native teams"
   homepage "https://juscribe.ai"
-  version "0.3.3"
+  version "0.3.4"
   license :cannot_represent
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/juscribe/jus-agent/releases/download/v#{version}/jus-agent-darwin-arm64"
-      sha256 "ae553fc4a36be68ad79dc12b225577afce38673262f5b66d61cb7d1d7b82ba8b"
+      sha256 "6ed494cd8a24232a8320860649e207384bf3cc7adb66b6ece3ce3ff549a3bd6c"
     else
       url "https://github.com/juscribe/jus-agent/releases/download/v#{version}/jus-agent-darwin-amd64"
-      sha256 "afd4f1d780f747917f4a5f2fb2e1947fbe734ee8510f8115d8b66bf28a38db5f"
+      sha256 "eaf9128854759b66f96c4beb37a3ea1be35e1d3805f7f895c363168328396027"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/juscribe/jus-agent/releases/download/v#{version}/jus-agent-linux-arm64"
-      sha256 "1ab7ceb9b8fc6665e47e5ce934d5503f9d7ea36baef0626cbeabfc83bd9d61b7"
+      sha256 "14699d24a2728161e781084cb1e638a295be00a2b8f142974896f8fdb508f99e"
     else
       url "https://github.com/juscribe/jus-agent/releases/download/v#{version}/jus-agent-linux-amd64"
-      sha256 "85c8096bd8909aff0123ef7dc5555261919c8d3c97dd97232721bb820f793fdf"
+      sha256 "810745b92a2ba2a6b117795af4ced70d802c8fc5b4c084a9bca1f99ab834443e"
     end
   end
 
   resource "jus-cli" do
-    url "https://github.com/juscribe/jus-agent/releases/download/v0.3.3/jus"
-    sha256 "efbb957292c7ccda308a54fd82f79fab6d9a6089cc697cd1475906bb5b2133a3"
+    url "https://github.com/juscribe/jus-agent/releases/download/v0.3.4/jus"
+    sha256 "9c71e4c82e4b74bf8bf2010c806d07340922a6b6493268744d765c597e676f2f"
   end
 
   def install
