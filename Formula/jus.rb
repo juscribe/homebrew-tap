22e6d21e926230b48401b4f67701da8d6ffd477e
caleon <caleon@juscribe.com>
2026-03-29 22:43:38 -0700

    jus-station changes

--- Formula/jus.rb
+++ Formula/jus.rb
@@ -6,33 +6,33 @@ class Jus < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/juscribe/jus-agent/releases/download/v#{version}/jus-agent-darwin-arm64"
+      url "https://github.com/juscribe/jus-station/releases/download/v#{version}/jus-station-darwin-arm64"
       sha256 "83e787f8f66ddaf3f984e49db5cd55289600c0a5cf9a4529e4d7612b101e18de"
     else
-      url "https://github.com/juscribe/jus-agent/releases/download/v#{version}/jus-agent-darwin-amd64"
+      url "https://github.com/juscribe/jus-station/releases/download/v#{version}/jus-station-darwin-amd64"
       sha256 "aa04280a4bd253ada5d111f82bb710a900798240349e0886755cc4cad670a8c0"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/juscribe/jus-agent/releases/download/v#{version}/jus-agent-linux-arm64"
+      url "https://github.com/juscribe/jus-station/releases/download/v#{version}/jus-station-linux-arm64"
       sha256 "d65bd787a139f52baea9a6a41f9b1e458775940d496fd7d70475cd75a9d865f6"
     else
-      url "https://github.com/juscribe/jus-agent/releases/download/v#{version}/jus-agent-linux-amd64"
+      url "https://github.com/juscribe/jus-station/releases/download/v#{version}/jus-station-linux-amd64"
       sha256 "ca56c2b42253af4323d178b750e027c7c20cb6bfd9239da93fd7fae2182fad60"
     end
   end
 
   resource "jus-cli" do
-    url "https://github.com/juscribe/jus-agent/releases/download/v0.4.0/jus"
+    url "https://github.com/juscribe/jus-station/releases/download/v#{version}/jus"
     sha256 "b0953d75067468385e1d6238a1b02ccaf410bad2eba0d05272aadaec16959664"
   end
 
   def install
     os = OS.mac? ? "darwin" : "linux"
     arch = Hardware::CPU.arm? ? "arm64" : "amd64"
-    bin.install "jus-agent-#{os}-#{arch}" => "jus-agent"
+    bin.install "jus-station-#{os}-#{arch}" => "jus-station"
 
     resource("jus-cli").stage do
       bin.install "jus"
@@ -41,6 +41,6 @@ class Jus < Formula
 
   test do
     assert_match "Usage:", shell_output("#{bin}/jus 2>&1", 1)
-    assert_match version.to_s, shell_output("#{bin}/jus-agent version 2>&1")
+    assert_match version.to_s, shell_output("#{bin}/jus-station version 2>&1")
   end
 end
