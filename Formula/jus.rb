40cd3cfc2067baf13bef383ac97fbc27b2abc585
caleon <caleon@users.noreply.github.com>
2026-09-23 22:35:32 -0700

    Update jus formula to v0.8.19

--- Formula/jus.rb
+++ Formula/jus.rb
@@ -1,32 +1,32 @@
 class Jus < Formula
   desc "Juscribe CLI — project management for AI-native teams"
   homepage "https://juscribe.ai"
-  version "0.8.18"
+  version "0.8.19"
   license :cannot_represent
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/juscribe/jus-dispatch/releases/download/v#{version}/jus-dispatch-darwin-arm64"
-      sha256 "6ab641eb1df38ce2afdd7d839ffec444e545c586cdb8efd0f8ce022c703a8cc1"
+      sha256 "fbb9bc7be10c07486a2d431e06155fbb9ff352a31162651447db255a491cf0ef"
     else
       url "https://github.com/juscribe/jus-dispatch/releases/download/v#{version}/jus-dispatch-darwin-amd64"
-      sha256 "d529838ece758b6723a75eb0530a3f19ee32d938fa09d7921d75bcd036639cc7"
+      sha256 "7a350feb9c30a6a5c2c0fce02de779333f88df2aa4ccd7b23e7c0e33f451842f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/juscribe/jus-dispatch/releases/download/v#{version}/jus-dispatch-linux-arm64"
-      sha256 "38d83161884de44e5b2ae1266d24e638b660d2d7436a6774e42d6e3165071249"
+      sha256 "d14956296167c81fe31c119fb3a4d63afc761e60abf2b325a6ce86620c3cdf2c"
     else
       url "https://github.com/juscribe/jus-dispatch/releases/download/v#{version}/jus-dispatch-linux-amd64"
-      sha256 "ba70511bd11adb62f7f9924f31c1df660bc2ff273e4beb74427178dd4cfcc45d"
+      sha256 "b0a783e8a272d697a1f191185692a688be4d25efd6d6a0da528ce833edadd926"
     end
   end
 
   resource "jus-cli" do
-    url "https://github.com/juscribe/jus-dispatch/releases/download/v0.8.18/jus"
-    sha256 "32b7c47b02dbeb4077ae36f4b428b74bb2c396294b31b50785fd70a21dcb3fd0"
+    url "https://github.com/juscribe/jus-dispatch/releases/download/v0.8.19/jus"
+    sha256 "9c8e995fa44f314e1e97198e9e02c42c1c85dd819491e7b4095252bae4491eea"
   end
 
   def install
